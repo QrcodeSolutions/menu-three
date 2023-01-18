40db10d2335294ed0172483c14a6f06e7de5bd9c
angeltried <angeltried@icloud.com>
2023-01-18 13:08:33 -0600

    menu 3

---
+++ app.js
@@ -0,0 +1,23 @@
+let left = document.getElementsByClassName('bi-arrow-left-short')[0]
+let right = document.getElementsByClassName('bi-arrow-right-short')[0]
+let cards = document.getElementsByClassName('cards')[0]
+
+left.addEventListener('click', ()=> {
+  cards.scrollLeft -= 140;
+})
+right.addEventListener('click', ()=> {
+  cards.scrollLeft += 140;
+})
+
+let poster = document.getElementById('poster');
+let title = document.getElementById('title');
+let price_cont = document.getElementById('price-cont');
+
+Array.from(document.getElementsByClassName('card')).forEach((ele, i )=>{
+  // console.log(ele) 
+  ele.addEventListener('click', ()=> {
+    poster.src = ele.getElementsByTagName('img')[0].src;
+    title.innerText = ele.getElementsByTagName('h5')[0].innerText;
+    price_cont.innerText = ele.getElementsByTagName('h4')[0].innerText;
+  })
+})
